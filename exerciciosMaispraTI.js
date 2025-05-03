5b43aefc68e31ce2faa6ea9a37ed48762b0341b0
Larissa3 <larissasilvamachado67@gmail.com>
2025-05-03 18:53:12 -0300

    Exercicio1.js

---
+++ exerciciosMaispraTI.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+// 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
+let num = 67; // Exemplo de número. Você pode substituir por outro valor.
+
+if (num % 2 === 0) {
+    console.log("o número é par");
+} else {
+    console.log("o número é ímpar");
+}
